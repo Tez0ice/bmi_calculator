8f7ac2a1c82b88b9b5ba00bf61fb930fd92e3500
Zayyad <muhdzayyad91@gmail.com>
2023-08-22 09:44:06 +0800

    Add message and Bmi Type

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   double _widthSliverValue = 100;
   double _heightsSliderValue = 100;
+  int bmi = 0;
+  String message = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,6 +42,7 @@ class _HomePageState extends State<HomePage> {
         body:Center(
           child: Column(
             children: [
+              const SizedBox(height: 20,),
               const Text("Bmi Calculator", style:TextStyle(color: Colors.red, fontSize: 32),),
               const SizedBox(height: 8,),
               const Text("We Care About Your Health"),
@@ -59,9 +63,32 @@ class _HomePageState extends State<HomePage> {
                 });
               }),
               const SizedBox(height: 8,),
-              TextButton.icon(onPressed:(){
+              bmi != 0 ? Text("Your BMI is $bmi and  $message") : const SizedBox(height: 2,),
+              TextButton.icon(onPressed: (){
                 var tempBmi = _widthSliverValue/pow((_heightsSliderValue/100),2);
                 print("BMI ${tempBmi.round()}");
+
+                //bmi = tempBmi.round();
+
+                var messageTemp = "";
+                if (tempBmi <18){
+                  messageTemp = "You are Underweight";
+                }
+                else if (tempBmi < 25){
+                  messageTemp = "You are normal";
+                }
+                else if (tempBmi < 30){
+                  messageTemp = "You are overweight";
+                }
+                else{
+                  messageTemp = "You are Obese";
+                }
+
+                setState((){
+                  bmi = tempBmi.round();
+                  message = messageTemp;
+                });
+
               }, icon:const Icon(Icons.favorite) , label:const Text("Calculate") )
             ],
           ),
